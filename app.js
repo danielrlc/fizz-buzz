7077d98ce97fab9e19fddebf785e16adeefd9c0e
thebluedaniel <daniel@Daniels-MacBook-Pro.local>
2014-10-03 11:15:28 +0300

    version 2

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
-for (i = 1; i <= 100; i++ ) {
-  if (i % 3 == 0 && i % 5 == 0) {
-    $("body").append("<p>fizz-buzz</p>");
+$(document).ready(function() {
+  var inputString = prompt("Pick a number from 1 to 100");
+  var inputNumber = +inputString;
+  if (isNaN(inputNumber)) {
+    alert("Choose a number! Refresh page and try again.");
+    resetPage();
   }
-  else if (i % 3 == 0) {
-    $("body").append("<p>fizz</p>");
+  else if (inputNumber % 1 != 0) {
+    alert("No decimal places allowed! Refresh page and try again.");
+    resetPage();
   }
-  else if (i % 5 == 0) {
-    $("body").append("<p>buzz</p>");
+  else if (inputNumber < 1) {
+    alert("The number must be 1 or higher! Refresh page and try again.");
+    resetPage();
   }
   else {
-    $("body").append("<p>" + i + "</p>");
+    function highestNumber(inputNumber) {
+      for (i = 1; i <= inputNumber; i++ ) {
+        if (i % 3 == 0 && i % 5 == 0) {
+          $("body").append("<p>fizz-buzz</p>");
+        }
+        else if (i % 3 == 0) {
+          $("body").append("<p>fizz</p>");
+        }
+        else if (i % 5 == 0) {
+          $("body").append("<p>buzz</p>");
+        }
+        else {
+          $("body").append("<p>" + i + "</p>");
+        }
+      }
+    }
   }
-}
+  highestNumber(inputNumber);
+});
